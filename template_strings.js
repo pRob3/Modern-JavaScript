f6797b3679ab9f808ccddf7804075c60fa7d5842
dennis.asplind <dennis.asplind@netonnet.com>
2022-09-22 07:07:10 +0200

    Template Strings

---
+++ template_strings.js
@@ -0,0 +1,15 @@
+const myname = 'John';
+
+const greeting = 'Hello, ' + myname + '!';
+
+const greeting2 = `Hello, ${myname}!`;
+
+const add = (a, b) => a + b;
+
+const message = `Hello, ${myname}
+
+How are you?
+
+The sum of 2+3 is ${add(2, 3)}`;
+
+console.log(message);
